9621232ef495c68a2d64cd4865e5bb13c826c2e0
Ingrid Santos <ils@cesar.org.br>
2019-07-23 14:36:32 -0400

    Shell Scripting

--- Alura/ShellScripting/Scripts/conversao-jpg-png.sh
+++ Alura/ShellScripting/Scripts/conversao-jpg-png.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-convert ~/Documentos/ESTUDOS/Alura/ShellScripting/imagens-livros/amazon_aws.jpg ~/Documentos/ESTUDOS/Alura/ShellScripting/imagens-livros/amazon_aws.png
+converte_imagem(){
+cd ~/Downloads/imagens-livros
+if [ ! -d png ]
+then
+    mkdir png
+fi
+
+for imagem in *.jpg
+do
+    local imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+done
+}
+
+converte_imagem
+
+echo $imagem_sem_extensao
